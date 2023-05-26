b3536645bad9e8fc2207916ed24e7790238d1791
Claire Villard <60586848+claire-villard-sonarsource@users.noreply.github.com>
2023-05-26 17:48:10 +0200

    Create Config.js

---
+++ src/Config.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "123456798-565121-qlkjb",
+    authDomain: "something.firebaseapp.com",
+    projectId: "something",
+    storageBucket: "something.appspot.com",
+    messagingSenderId: "368436153873",
+    appId: "1:5357343:web:35754873645"
+  };
