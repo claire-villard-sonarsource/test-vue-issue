0923b9a7942ab4ca9943409a0438c6fceb72edde
Claire Villard <claire.villard@sonarsource.com>
2026-01-09 15:23:36 +0100

    New space

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import RwvHeader from "@/components/TheHeader";
 import RwvFooter from "@/components/TheFooter";
 
 export default {
-  name: "TheApp",
+    name: "TheApp",
   components: {
     RwvHeader,
     RwvFooter
